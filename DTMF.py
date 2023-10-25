fd8bc42c460862ab0e2bb07a2a5dc9ff40064289
Juan Diego Bermudez Castañeda <juan052115@gmail.com>
2023-10-25 13:25:03 -0500

    funciones para generar el tono como combinacion de las frecuencias, funcion para detectar el numero segun el tono

--- DTMF.py
+++ DTMF.py
@@ -1,4 +1,4 @@
-def generar_audio(frec):
+def generar_audio(frec, file_name):
     import numpy as np
     from scipy.io import wavfile
 
@@ -16,13 +16,13 @@ def generar_audio(frec):
     tono = amplitud * np.sin(2 * np.pi * frecuencia * tiempo)
 
     # Guardar el tono como un archivo WAV
-    nombre_archivo = "1.wav"
+    nombre_archivo = file_name
     wavfile.write(nombre_archivo, 44100, tono.astype(np.float32))
 
     print(f"Tono de {frecuencia} Hz guardado como {nombre_archivo}")
 
 
-def obtener_frecuencias():
+def obtener_frecuencias(file_name):
     import numpy as np
     import matplotlib.pyplot as plt
     from scipy.io import wavfile
@@ -42,14 +42,14 @@ def obtener_frecuencias():
         return y
 
     # Cargar archivo de audio
-    samplerate, data = wavfile.read("audio.wav")
+    samplerate, data = wavfile.read(file_name)
 
     # Convertir a mono si es estéreo
     if len(data.shape) > 1:
         data = np.mean(data, axis=1)
 
     # Aplicar filtro pasabajas
-    cutoff = 1000  # Frecuencia de corte en Hz
+    cutoff = 1800  # Frecuencia de corte en Hz
     filtered_data = butter_lowpass_filter(data, cutoff, samplerate)
 
     # Obtener la transformada de Fourier
@@ -59,7 +59,7 @@ def obtener_frecuencias():
     abs_fft = np.abs(fft_out)
 
     # Encontrar las 3 frecuencias más altas
-    frequencies = np.argsort(abs_fft)[-3:]
+    frequencies = np.argsort(abs_fft)[-2:]
 
     # Escalar las frecuencias para obtenerlas en Hz
     frequencies_hz = frequencies * samplerate / len(filtered_data)
@@ -72,8 +72,92 @@ def obtener_frecuencias():
     plt.title("Spectrum")
     plt.xlabel("Frequency (Hz)")
     plt.ylabel("Magnitude")
+    plt.grid()
+    # poner un punto en cada frecuencia principal
+    for f in frequencies_hz:
+        plt.plot(f, abs_fft[int(f * len(filtered_data) / samplerate)], "ro")
     plt.show()
 
+    return frequencies_hz
 
-# obtener_frecuencias()
-generar_audio(frec=696.70834749)
+
+def generar_tono_dtmf(archivo_wav, output_file_name):
+    import soundfile as sf
+    import numpy as np
+    # Obtener las dos frecuencias de la DTMF
+    frecuencias = obtener_frecuencias(archivo_wav)
+    frecuencia1 = frecuencias[0]
+    frecuencia2 = frecuencias[1]
+
+    # Generar los tonos de cada frecuencia y guardarlos como archivos WAV
+    generar_audio(frecuencia1, "tono1.wav")
+    generar_audio(frecuencia2, "tono2.wav")
+
+    # Combinar los dos archivos WAV para crear el tono de la DTMF completo
+
+    # Cargar los archivos WAV
+    tono1, samplerate = sf.read("tono1.wav")
+    tono2, samplerate = sf.read("tono2.wav")
+
+    # Alinear los dos tonos
+    diferencia = len(tono2) - len(tono1)
+    if diferencia > 0:
+        tono1 = np.pad(tono1, (0, diferencia), "constant")
+    else:
+        tono2 = np.pad(tono2, (0, -diferencia), "constant")
+
+    # Combinar los dos tonos
+    tono_completo = tono1 + tono2
+
+    # Guardar el tono completo como un archivo WAV
+    sf.write(output_file_name, tono_completo, samplerate)
+
+# generar_tono_dtmf('Dtmf1.wav', 'tono_1.wav')
+# generar_tono_dtmf('Dtmf2.wav', 'tono_2.wav')
+# generar_tono_dtmf('Dtmf3.wav', 'tono_3.wav')
+# generar_tono_dtmf('Dtmf4.wav', 'tono_4.wav')
+# generar_tono_dtmf('Dtmf5.wav', 'tono_5.wav')
+
+def obtener_numero_presionado(archivo_wav):
+    import numpy as np
+    from scipy.io import wavfile
+    # Obtener las dos frecuencias del tono DTMF
+    samplerate, data = wavfile.read(archivo_wav)
+    frecuencias = np.array(obtener_frecuencias(archivo_wav))
+
+    # Definir la tabla de frecuencias DTMF
+    tabla_frecuencias = {
+        (697, 1209): "1",
+        (697, 1336): "2",
+        (697, 1477): "3",
+        (697, 1633): "A",
+        (770, 1209): "4",
+        (770, 1336): "5",
+        (770, 1477): "6",
+        (770, 1633): "B",
+        (852, 1209): "7",
+        (852, 1336): "8",
+        (852, 1477): "9",
+        (852, 1633): "C",
+        (941, 1209): "*",
+        (941, 1336): "0",
+        (941, 1477): "#",
+        (941, 1633): "D",
+    }
+
+    # Buscar las dos frecuencias en la tabla de frecuencias
+    for frecuencia1, frecuencia2 in tabla_frecuencias.keys():
+        if (
+            abs(frecuencias[0] - frecuencia1) <= 10
+            and abs(frecuencias[1] - frecuencia2) <= 10
+        ) or (
+            abs(frecuencias[0] - frecuencia2) <= 10
+            and abs(frecuencias[1] - frecuencia1) <= 10
+        ):
+            return tabla_frecuencias[(frecuencia1, frecuencia2)]
+
+    # Si no se encontraron dos frecuencias, devolver None
+    return None
+
+numero = obtener_numero_presionado('tono_5.wav')
+print(numero)
